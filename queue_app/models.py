26ea5dcf788967ad6f8e08174aae59cb466a1139
Natcha Ranchan <natcha.ran@ku.th>
2024-10-11 14:58:21 +0700

    index only show business within open hours

--- queue_app/models.py
+++ queue_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.utils import timezone
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -20,6 +20,50 @@ class Business(models.Model):
     open_status = models.CharField(max_length=50)
     field_name = models.CharField(max_length=100, default=None, null=True)
     field_choice = models.CharField(max_length=350)
+
+    DAYS = {0: 'Monday',
+            1: 'Tuesday',
+            2: 'Wednesday',
+            3: 'Thursday',
+            4: 'Friday',
+            5: 'Saturday',
+            6: 'Sunday'}
+
+    @staticmethod
+    def convert_to_list(my_str) -> list[str]:
+        return [i.strip() for i in my_str.split(',')]
+
+    def is_approved(self) -> bool:
+        """Check if the business was approved by admin
+        Return: bool
+        """
+        return self.approve_status == "Approved"
+
+    def is_opened(self):
+        """Check if the business is in condition to book a queue.
+        Return: bool: True if the business is open today,
+        the business has already been approved by admin,
+        and the current time falls between opening and ending times.
+        """
+
+        days_lst = Business.convert_to_list(self.open_day)
+
+        today_day_int = timezone.now().weekday()
+
+        try:
+            today_day = Business.DAYS[today_day_int]
+        except KeyError:
+            return False
+
+        now = timezone.localtime(timezone.now()).time()
+
+        # same day open and close
+        if now > self.open_time:
+            check_time = self.open_time <= now <= self.close_time
+        else:  # edge case
+            check_time = self.open_time >= now and self.close_time >= now
+        return (today_day in days_lst and check_time
+                and self.is_approved() and self.open_status == 'Open')
 
     def __str__(self):
         return self.name
--- queue_app/views.py
+++ queue_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.views import generic
+from .models import Business
 # Create your views here.
+
+
+class IndexView(generic.ListView):
+    template_name = "queue_app/index.html"
+    context_object_name = "business_list"
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Business.objects.order_by('name')
+
+# def home(request):
+#     context = {'business_list' : Business.objects.all()}
+#     return render(request, 'index.html', context)
