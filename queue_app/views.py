fe6cccf2fb32606d2b6dffe79f1ba4013e897e63
Natcha Ranchan <natcha.ran@ku.th>
2024-10-11 22:39:27 +0700

    Edit docstring

--- queue_app/views.py
+++ queue_app/views.py
@@ -4,10 +4,10 @@ from .models import Business
 
 
 class IndexView(generic.ListView):
-    """generic index view for page home"""
+    """Generic index view for page home"""
     template_name = "queue_app/index.html"
     context_object_name = "business_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
+        """Return business by their alphabet."""
         return Business.objects.order_by("name")
