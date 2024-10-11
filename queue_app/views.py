d200ac3cef728aa1132f6fa3c08dfba9754b215c
Thanchida <thanchida.a@ku.th>
2024-10-11 15:32:57 +0700

    Add view to show and save business register form

--- queue_app/views.py
+++ queue_app/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import BusinessRegisterForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
-# Create your views here.
+
+@login_required
+def business_register(request):
+    if request.method == 'POST':
+        form = BusinessRegisterForm(request.POST)
+        if form.is_valid():
+            business = form.save(commit=False)
+            business.user = request.user
+            business.save()
+            messages.success(request,
+                             f"{request.user.username}, "
+                             f"your request for {business.name} has been successfully submitted!")
+            return redirect('queue_app:home')
+    else:
+        form = BusinessRegisterForm()
+    return render(request, 'queue_app/business_register.html', {'form': form})
+
