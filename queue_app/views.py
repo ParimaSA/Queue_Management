8655d49cec0fd7164318591da7d90eec13bde9e9
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-12 02:28:59 +0700

    Add view for reserve queue.

--- queue_app/views.py
+++ queue_app/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Business, Queue
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.utils import timezone
+from django.http import Http404
 
-# Create your views here.
+
+@method_decorator(login_required, name='dispatch')
+class ReserveQueueView(View):
+    """
+    Handles queue reservation for a business.
+    """
+    def get(self, request, business_id):
+        """
+        Displays the reservation options for the given business.
+        """
+        try:
+            business = Business.objects.get(id=business_id)
+        except Business.DoesNotExist:
+            raise Http404("Business does not exist.")
+
+        field_choices = business.field_choice.split(',')
+        context = {
+            'business': business,
+            'field_choices': field_choices,
+        }
+        return render(request, 'queue_app/reserve_queue.html', context)
+
+    def post(self, request, business_id):
+        """
+        Processes the queue reservation for the authenticated user.
+        """
+        try:
+            business = Business.objects.get(id=business_id)
+        except Business.DoesNotExist:
+            raise Http404("Business does not exist.")
+
+        field_choice = request.POST.get('field_choice')
+        reserve_time = timezone.now()
+
+        Queue.objects.create(
+            user=request.user,
+            business=business,
+            field_choice=field_choice,
+            reserve_time=reserve_time,
+            status="Pending"
+        )
+
+        return redirect('queue_app:index')  # To be updated to another page like in_queue
