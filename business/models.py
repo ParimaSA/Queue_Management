6b48a6649efabdadf5b027da23b8765237792c92
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-10-13 12:33:58 +0700

    Implement model for business and customer application

--- business/models.py
+++ business/models.py
@@ -1,4 +1,40 @@
 """Provide model using in business app."""
+from django import forms
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+class Business(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+
+class BusinessSignupForm(forms.ModelForm):
+    name = forms.CharField(max_length=255)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            Business.objects.create(user=user, business_name=self.cleaned_data['business_name'])
+        return user
+
+
+class Queue(models.Model):
+    business = models.ForeignKey(Business, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    estimated_time = models.IntegerField()
+
+
+class Entry(models.Model):
+    name = models.CharField(max_length=50)
+    tracking_code = models.CharField(max_length=50)
+    time_in = models.DateTimeField(default=timezone.now)
+    time_out = models.DateTimeField()
+    status = models.CharField(max_length=20)
--- customer/models.py
+++ customer/models.py
@@ -1,1 +1,27 @@
 """Provide model using in customer app."""
+from django import forms
+from django.db import models
+from django.contrib.auth.models import User
+from business.models import Entry
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class CustomerSignupForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+            Customer.objects.create(user=user)  # Create a customer profile
+        return user
+
+
+class CustomerQueue(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
