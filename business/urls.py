54140534f142ef9b116ff57515348e5a3ecad391
Thanchida <thanchida.a@ku.th>
2024-10-16 14:14:18 +0700

    Implement URL routing for business queue entry page

--- business/urls.py
+++ business/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('', RedirectView.as_view(url='home/', permanent=False)),
+    path('<int:pk>/businessEntry/', views.show_entry, name='businessEntry'),
+    path('<int:pk>/addQueue/', views.add_queue, name='addQueue'),
+    path('<int:pk>/runQueue/', views.run_queue, name='runQueue'),
 ]
