7da42363dfd7408ad92059d98c61c4583aba4e2c
Thanchida <thanchida.a@ku.th>
2024-10-16 17:13:00 +0700

    Change name of URL for routing for business queue entry page

--- business/urls.py
+++ business/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('', RedirectView.as_view(url='home/', permanent=False)),
     path('<int:pk>/businessEntry/', views.show_entry, name='businessEntry'),
-    path('<int:pk>/addQueue/', views.add_queue, name='addQueue'),
-    path('<int:pk>/runQueue/', views.run_queue, name='runQueue'),
+    path('<int:pk>/addQueue/', views.add_queue, name='add_queue'),
+    path('<int:pk>/editQueue/', views.edit_queue, name='edit_queue'),
+    path('<int:pk>/runQueue/', views.run_queue, name='run_queue'),
 ]
