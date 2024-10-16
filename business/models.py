aa8ea7d98e51715f1389e72d8be8dd90f3e8b613
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-16 12:29:50 +0700

    Refactor Queue and Entry models for tracking code and entry name generation

--- business/models.py
+++ business/models.py
@@ -3,6 +3,7 @@ from django import forms
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from nanoid import generate
 
 
 class Business(models.Model):
@@ -32,7 +33,8 @@ class BusinessSignupForm(forms.ModelForm):
 class Queue(models.Model):
     business = models.ForeignKey(Business, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
-    estimated_time = models.IntegerField(default=None)
+    alphabet = models.CharField(max_length=1, default='A')
+    estimated_time = models.IntegerField(default=None, null=True)
 
     def __str__(self):
         return self.name
@@ -40,11 +42,33 @@ class Queue(models.Model):
 
 class Entry(models.Model):
     name = models.CharField(max_length=50)
-    queue_name = models.ForeignKey(Queue, on_delete=models.CASCADE, null=True)
-    tracking_code = models.CharField(max_length=50)
+    queue = models.ForeignKey(Queue, on_delete=models.CASCADE, null=True)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, null=True)
+    tracking_code = models.CharField(max_length=50, unique=True, null=True, blank=True)
     time_in = models.DateTimeField(default=timezone.now)
-    time_out = models.DateTimeField()
-    status = models.CharField(max_length=20)
+    time_out = models.DateTimeField(null=True)
+    status = models.CharField(max_length=20, default='waiting')
+
+    def save(self, *args, **kwargs):
+        if not self.tracking_code and self.status != 'completed':
+            while True:
+                new_tracking_code = generate('1234567890abcdefghijklmnopqrstuvwxyz', size=10)
+                if not Entry.objects.filter(tracking_code=new_tracking_code, time_out__isnull=True).exists():
+                    self.tracking_code = new_tracking_code
+                    break
+
+        if not self.name:
+            today = timezone.now().date()
+            queue_entries_today = Entry.objects.filter(queue=self.queue, time_in__date=today).count() + 1
+            self.name = f"{self.queue.alphabet}{queue_entries_today}"
+
+        super().save(*args, **kwargs)
+
+    def mark_as_completed(self):
+        self.status = 'completed'
+        self.time_out = timezone.now()
+        self.tracking_code = None
+        self.save()
 
     def __str__(self):
         return self.name
