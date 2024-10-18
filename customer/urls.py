87657bb87901faf17341d11c4102d014fe719619
Natcha Ranchan <natcha.ran@ku.th>
2024-10-19 02:13:28 +0700

    Remove temporary login.

--- customer/urls.py
+++ customer/urls.py
@@ -6,8 +6,7 @@ app_name = 'customer'
 urlpatterns = [
     path('home/', views.HomeListView.as_view(), name='home'),
     path('profile/', views.profile, name='profile'),
-    # path('login/', views.login, name='login'),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('', RedirectView.as_view(url='home/', permanent=False)),
     path('<int:entry_id>/cancel_queue/', views.cancel_queue, name='cancel-queue')
