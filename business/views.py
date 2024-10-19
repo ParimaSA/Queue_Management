6ba3bebf33e298c7b72442edbe6fc60aedd62a44
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-19 13:07:52 +0700

    Update home and add_customer views to handle business_id

--- business/views.py
+++ business/views.py
@@ -1,14 +1,41 @@
 """Views for business app."""
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse
+from .models import Business, Queue, Entry
+from django.utils import timezone
 
 
-def home(request):
-    return render(request, 'business/home.html')
+def home(request, business_id):
+    business = get_object_or_404(Business, id=business_id)
+    context = {'business': business}
+    return render(request, 'business/home.html', context)
 
 
-def add_customer(request):
-    return render(request, 'business/add_customer.html')
+def add_customer(request, business_id):
+    business = get_object_or_404(Business, id=business_id)
+    queues = Queue.objects.filter(business=business)
+
+    tracking_code = None
+
+    if request.method == 'POST':
+        queue_id = request.POST.get('queue')
+        selected_queue = get_object_or_404(Queue, id=queue_id)
+
+        entry = Entry(
+            queue=selected_queue,
+            business=business,
+            time_in=timezone.now()
+        )
+        entry.save()
+
+        tracking_code = entry.tracking_code
+
+    return render(request, 'business/add_customer.html', {
+        'business': business,
+        'queues': queues,
+        'tracking_code': tracking_code
+    })
 
 
 def queue(request):
