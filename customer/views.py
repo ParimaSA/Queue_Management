472aa7a6e1b55e8a90b55b01b722be0a41d8a78f
Natcha Ranchan <natcha.ran@ku.th>
2024-10-19 17:31:00 +0700

    Home page working with visitors and customers

--- customer/views.py
+++ customer/views.py
@@ -1,8 +1,9 @@
 """Views for customer app."""
+
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
-from .models import CustomerQueue,Customer, Entry
+from .models import CustomerQueue, Customer, Entry
 from django.contrib import messages
 import logging
 
@@ -11,37 +12,62 @@ logger = logging.getLogger(__name__)
 
 
 def cancel_queue(request, entry_id):
+    """When the queue is canceled, the entry is also cancel."""
     Entry.objects.get(id=entry_id).delete()
-    return redirect('customer:home')
+    return redirect("customer:home")
+
 
 class HomeListView(ListView):
-    template_name = 'customer/home.html'
-    context_object_name = 'customer_queue_list'
+    """View for customer's home page."""
+
+    template_name = "customer/home.html"
+    context_object_name = "customer_queue_list"
 
     def get_queryset(self):
+        """Return a queryset of CustomerQueue instances for the authenticated user.
+
+        If it's anonymous user, it will return nothing.
+        """
+        if not self.request.user.is_authenticated:
+            return []
         return CustomerQueue.objects.filter(customer__user__username=self.request.user)
 
-
     def post(self, request, *args, **kwargs):
-        track_code = request.POST['track-code']
+        """Allow an authenticated user can cancel and view their customer queue by inputting track code.
+
+        Visitor can only view a single entry based on track code.
+        In case another user tried to access the track code of others,
+        they cannot view the entry nor cancel it either.
+        """
+        print(request.user.username)
+
+        track_code = request.POST["track-code"]
         try:
             my_entry = Entry.objects.get(tracking_code=str(track_code))
         except (Entry.DoesNotExist, KeyError):
-            logger.error(f'trackcode {track_code} does not exist in entry')
-            return redirect('customer:home')
+            logger.error(f"Track code: {track_code} does not exist in entry")
+            messages.warning(request, "The track code is incorrect.")
+            return redirect("customer:home")
 
-        logger.info(f'trackcode {track_code} existed in entry.')
+        logger.info(f"Trackcode: {track_code} existed in entry.")
+        if not self.request.user.is_authenticated:  # visitors
+            logger.info("Visitor tried to see entry.")
+            return render(
+                request, self.template_name, {"customer_queue_list": [my_entry]}
+            )
+
         try:
-            entry_count = CustomerQueue.objects.filter(customer__user=self.request.user, entry=my_entry).count()
+            my_queue = CustomerQueue.objects.get(entry=my_entry)
+            if my_queue.customer.user != self.request.user:
+                logger.warning("This user tried to access other person queue entry.")
+                messages.warning(request, "You can't access someone else entry!")
+        except CustomerQueue.DoesNotExist:
             my_customer = Customer.objects.get(user=self.request.user)
-        except (Customer.DoesNotExist):
-            logger.error(f'{self.request.user.username} is not customer')
-            return redirect('customer:home')
-        if not entry_count:
             CustomerQueue.objects.create(customer=my_customer, entry=my_entry)
-            logger.info(f'create customer_queue for {self.request.user}')
+            logger.info(f"create customer_queue for {self.request.user.username}")
+            messages.success(request, "This entry is added to your queue history.")
 
-        return redirect('customer:home')
+        return redirect("customer:home")
 
 
 def profile(request):
