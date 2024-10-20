30b423416e0618a3f1408f0fc3987c5f3acf052e
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-10-20 11:21:47 +0700

    Remove business_id from URLs for home and add_customer

--- business/urls.py
+++ business/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 app_name = 'business'
 urlpatterns = [
-    path('home/<int:business_id>/', views.home, name='home'),
-    path('add_customer/<int:business_id>/', views.add_customer, name='add_customer'),
+    path('home/', views.home, name='home'),
+    path('add_customer/', views.add_customer, name='add_customer'),
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
     path('', RedirectView.as_view(url='home/', permanent=False)),
