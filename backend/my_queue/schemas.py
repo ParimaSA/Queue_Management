451de3b03c21b4e482a4207de1004c4a8ed46119
Natcha Ranchan <natcha.ran@ku.th>
2024-10-30 12:12:07 +0700

    Add schemas for api.

---
+++ backend/my_queue/schemas.py
@@ -0,0 +1,38 @@
+from typing import Optional
+from datetime import datetime
+from ninja import Schema, ModelSchema
+from .models import Business
+
+
+class BusinessSchema(ModelSchema):
+    class Meta:
+        model = Business
+        fields = ('user', 'name')
+
+
+class QueueSchema(Schema):
+    # GET
+    id: int
+    name: str
+    estimated_time: int = None
+
+
+class EntryDetailSchema(Schema):
+    id: int
+    name: str
+    queue: QueueSchema
+    business: str
+    tracking_code: Optional[str]
+    time_in: datetime
+    time_out: Optional[datetime]
+    status: str = "waiting"
+    queue_ahead: int
+
+
+class EditIn(Schema):
+    name: str
+    alphabet: str
+
+
+class CustomerQueueCreateSchema(Schema):
+    tracking_code: str
