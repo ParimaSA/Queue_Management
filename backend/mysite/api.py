696676923ea7d67ab873ddbb439e68dc1367995b
Natcha Ranchan <natcha.ran@ku.th>
2024-10-30 12:54:52 +0700

    Register controllers to mysite's api.

--- backend/mysite/api.py
+++ backend/mysite/api.py
@@ -4,7 +4,17 @@ from ninja import NinjaAPI, Schema
 from ninja_jwt.authentication import JWTAuth
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.controller import NinjaJWTDefaultController
+from my_queue.api import BusinessController, QueueController, EntryController
 
 
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
+api.register_controllers(BusinessController, QueueController, EntryController)
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+
+@api.get("me/", response=UserSchema)
+def who(request):
+    return request.user
