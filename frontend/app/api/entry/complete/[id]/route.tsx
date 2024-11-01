492d75374a23fe8018652e4effb618ab145bcad2
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-01 17:25:45 +0700

    Edit cancel and complete route in entry.

--- frontend/app/api/entry/complete/[id]/route.tsx
+++ frontend/app/api/entry/complete/[id]/route.tsx
@@ -7,13 +7,13 @@ import ApiProxy from "@/app/api/proxy";
 const DJANGO_API_ENTRY_URL = `${DJANGO_API_ENDPOINT}/entry`;
 
 export async function POST(request: Request, { params }: { params: { id: string } }) {
-    const endpoint = `${DJANGO_API_ENTRY_URL}/${params.id}/status/cancel` ;
+    const endpoint = `${DJANGO_API_ENTRY_URL}/${params.id}/status/complete` ;
 
     try {
         const { data, status }: { data: any; status: number } = await ApiProxy.post(endpoint, null, true);
         return NextResponse.json(data, { status });
     } catch (error) {
-        console.error("Error cancel entry:", error);
-        return NextResponse.json<{ error: string }>({ error: "Failed to cancel entry" }, { status: 500 });
+        console.error("Error completing entry:", error);
+        return NextResponse.json<{ error: string }>({ error: "Failed to complete entry" }, { status: 500 });
     }
 }
