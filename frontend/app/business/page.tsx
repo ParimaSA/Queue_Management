6a06aaac523147c709968892294c070caac378f6
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-01 18:06:22 +0700

    Improve the redirect when the user is and is not authenticated

--- frontend/app/business/page.tsx
+++ frontend/app/business/page.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+"use client"
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import { useAuth } from '@/components/authProvider'
 import BusinessPage from './ShowEntry'
 import BusinessNavbar from './components/BusinessNavbar'
 
 const Business = () => {
+  const auth = useAuth()
+  const router = useRouter()
+
+  // Redirect if the user is not authenticated
+  useEffect(() => {
+    if (!auth.isAuthenticated) {
+        router.replace('/business/login')
+    }
+  }, [auth.isAuthenticated, router])
+
   return (
     <main>
         <BusinessNavbar/>
