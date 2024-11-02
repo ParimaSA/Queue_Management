0c0f4a8b23b9a9fce5c9ca64362d366b36c8006b
Natcha Ranchan <natcha.ran@ku.th>
2024-11-03 01:14:07 +0700

    Base class for unittests.

---
+++ backend/my_queue/tests/base.py
@@ -0,0 +1,27 @@
+import json
+from django.test import TestCase
+from ninja.testing import TestClient
+from django.contrib.auth.models import User
+from my_queue.models import Business, Queue
+from mysite.api import api
+
+
+class BaseTestCase(TestCase):
+    client = TestClient(api)
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='test1234')
+        self.business = Business.objects.create(user=self.user, name='sushiro')
+        self.queue = Queue.objects.create(name='Test Queue', business=self.business)
+
+    def login(self, username, password):
+        response_token = self.client.post(
+            '/api/token/pair',
+            data=json.dumps({
+                'username': username,
+                'password': password
+            }),
+            content_type='application/json'
+        )
+        token = response_token.json().get('access')
+        return token
