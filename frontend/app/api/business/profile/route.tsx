f49e8fc9c68743ed2ed48bdd84da3b14984087e9
Natcha Ranchan <natcha.ran@ku.th>
2024-11-15 13:16:35 +0700

    Remove comments

--- frontend/app/api/business/profile/route.tsx
+++ frontend/app/api/business/profile/route.tsx
@@ -3,7 +3,6 @@
 import { DJANGO_API_ENDPOINT } from "@/config/defaults";
 import { NextResponse } from "next/server";
 import ApiProxy from "@/app/api/proxy";
-// import ApiProxy2 from "@/app/api/proxy2";
 
 const DJANGO_API_BUSINESS_PROFILE_URL = `${DJANGO_API_ENDPOINT}/business/profile`;
 const DJANGO_API_BUSINESS_UPDATE_PROFILE_URL = `${DJANGO_API_ENDPOINT}/business/profile`
@@ -23,26 +22,6 @@ export async function GET(request: Request) {
 }
 
 
-
-// export async function PUT(request: Request) {
-//     const endpoint = DJANGO_API_BUSINESS_UPDATE_PROFILE_URL;
-
-//     // const requestData = await request.json();
-//     const formData = await request.formData();
-//     const files = formData.getAll('files') as File[];
-//     console.log("Files:", files);
-
-//     try {
-//         const { data, status }: { data: any; status: number } = await ApiProxy.put(endpoint, files, true);
-//         return NextResponse.json<any>(data, { status });
-//     } catch (error) {
-//         console.error("Error updating business:", error);
-//         return NextResponse.json<ErrorResponse>({ error: "Failed to update business" }, { status: 500 });
-//     }
-// }
-
-
-
 export async function POST(request: Request) {
     const endpoint = DJANGO_API_BUSINESS_UPDATE_PROFILE_URL;
 
@@ -52,9 +31,6 @@ export async function POST(request: Request) {
     const file: File | null  = formData.get('file') as File;
     console.log("Form data:", formData);
     console.log("File:", file);
-    // Prepare FormData to send to Django API
-    // const formDataToSend = new FormData();
-    // formDataToSend.append('profile_image', file); // Ensure 'profile_image' matches the field name expected by Django
 
     try {
         // Send FormData to Django API using ApiProxy
