691890427a8cde778229d17e2fcb1d60600eaf3b
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-17 20:00:40 +0700

    Add test for login

---
+++ frontend/tests/business-login.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test';
+
+test('Business Owner Login and Check Profile', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Business Owner' }).click();
+
+  // Login
+  await page.getByPlaceholder('Username').fill('owner1');
+  await page.getByPlaceholder('Password').fill('hackme11');
+  await page.getByRole('button', { name: 'Login' }).click();
+
+  await page.waitForURL('http://localhost:3000/business');
+  await expect(page).toHaveURL('http://localhost:3000/business');
+
+  // Go to the profile page
+  await page.getByText('Account').click();
+  await page.getByRole('link', { name: 'Profile' }).click();
+
+  await page.waitForURL('http://localhost:3000/business/profile');
+  await expect(page).toHaveURL('http://localhost:3000/business/profile');
+
+  // Have correct business name
+  const element = page.locator('text="TestRestaurant"');
+  await expect(element).toHaveText('TestRestaurant');
+
+});
