28d153111346249f68cc4c2fd8ac250c20c2a83f
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-21 14:57:38 +0700

    Remove unused parameter in entry post method

--- frontend/app/api/entry/[id]/route.tsx
+++ frontend/app/api/entry/[id]/route.tsx
@@ -7,11 +7,11 @@ import ApiProxy from "../../proxy";
 const DJANGO_API_ENTRY_URL = `${DJANGO_API_ENDPOINT}/entry/tracking-code`;
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-    const { id } = params; 
+    const { id } = await params; 
     const endpoint = `${DJANGO_API_ENTRY_URL}/${id}`;
 
     try {
-        const { data, status }: { data: any; status: number } = await ApiProxy.get(endpoint, null, true);
+        const { data, status }: { data: any; status: number } = await ApiProxy.get(endpoint, true);
         return NextResponse.json(data, { status });
     } catch (error) {
         console.error("Error completing entry:", error);
