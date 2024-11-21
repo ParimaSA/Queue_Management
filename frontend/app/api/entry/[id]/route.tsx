ca37890cb8cc2a984bb086635bff2a029a01a5be
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-21 14:40:54 +0700

    Edit the parameter in entry route

--- frontend/app/api/entry/[id]/route.tsx
+++ frontend/app/api/entry/[id]/route.tsx
@@ -7,8 +7,8 @@ import ApiProxy from "../../proxy";
 const DJANGO_API_ENTRY_URL = `${DJANGO_API_ENDPOINT}/entry/tracking-code`;
 
 export async function GET(request: Request, { params }: { params: { id: string } }) {
-    const { id } = await params
-    const endpoint = `${DJANGO_API_ENTRY_URL}/${id}` ;
+    const { id } = params; 
+    const endpoint = `${DJANGO_API_ENTRY_URL}/${id}`;
 
     try {
         const { data, status }: { data: any; status: number } = await ApiProxy.get(endpoint, null, true);
