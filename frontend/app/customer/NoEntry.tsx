e4cba1acaf65ecc7d7f93b9af677c4111f3c9bad
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-23 03:24:11 +0700

    Improve wording in No Entry page

--- frontend/app/customer/NoEntry.tsx
+++ frontend/app/customer/NoEntry.tsx
@@ -6,8 +6,8 @@ const NoEntry: React.FC = () => {
   return (
     <div className="bg-cream2 w-screen h-screen flex justify-center items-center">
         <div className="bg-red-100 rounded-lg shadow-lg p-10 max-w-sm w-full text-center border-2 border-none">
-            <h1 className='text-xl text-red-900 font-bold'>No Entry Found</h1>
-            <h2 className='text-l text-red-700 font-bold '>Contact the business to get an entry.</h2>
+            <h1 className='text-2xl text-red-900 font-bold'>No queue entry found.</h1>
+            <h2 className='text-l text-red-700 font-bold mt-3 '>Please visit the business to request details or a new queue entry.</h2>
         </div>
     </div>
   );
