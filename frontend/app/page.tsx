eef2a72bce58ca4d1ef8cd45ec9fcf975b68237e
Thanchida <thanchida.a@ku.th>
2024-11-23 16:51:09 +0700

    Fix layout in home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,28 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import {fredoka} from "./fonts/fonts";
+import React from 'react';
+import Link from 'next/link';
+import { fredoka } from './fonts/fonts';
+
 const Home = () => {
   return (
     <>
-    <main className="flex flex-col min-h-screen lg:p-20 md:p-15 sm:p-10 lg:py-40 md:py-32 sm:py-20 lg:px-32 md:px-20 sm:px-10 bg-cream2">
-        <div className={`${fredoka.className} antialiased`}>
-          <h1 className="lg:text-8xl md:text-6xl sm:text-5xl font-bold text-darkBlue text-left">Queue</h1>
-          <h1 className="lg:text-8xl md:text-6xl sm:text-5xl font-bold text-lightBlue4 text-left lg:mt-4">Management</h1>
-          <p className="lg:text-3xl md:text-2xl sm:text-2xl text-lightPurple lg:mt-8 text-left">
-            You can choose your role to start using our website
+     <main className="flex flex-col justify-center items-center min-h-screen bg-cream2 lg:px-32 md:px-20 sm:px-10">
+      <div className={`${fredoka.className} antialiased px-6 py-16 text-center`}>
+        <div className="bg-white shadow-lg rounded-3xl p-6 md:p-14 lg:p-16 relative -translate-y-8 w-full max-w-5xl">
+          <h1 className="text-5xl md:text-6xl lg:text-7xl font-extrabold tracking-tight text-darkBlue">
+            Queue
+            <span className="text-lightBlue4 block">Management</span>
+          </h1>
+          <p className="mt-8 text-lg md:text-xl lg:text-2xl text-lightPurple font-medium">
+            Keep your business organized and your customers happy with our queue management solution.
           </p>
-          <div className='lg:grid lg:grid-cols-2 lg:gap-4 lg:mt-14 md:grid md:grid-cols-2 md:gap-4 md:mt-14'>
-             <Link href="/customer">
-                <button className='btn btn-primary w-full text-xl text-white bg-lightPink border-none hover:bg-pink-900 sm:mt-3 md:mt-0 lg:mt-0'>Customer</button>
-              </Link>
-              <Link href="/business/login">
-                <button className='btn btn-primary w-full text-xl text-white bg-lightPurple3 border-none hover:bg-purple-900 sm:mt-3 md:mt-0 lg:mt-0'>Business Owner</button>
-              </Link>
+          <div className="mt-12 flex justify-center">
+            <Link href="/business/login">
+              <button className="w-60 lg:w-72 px-8 py-4 text-lg font-semibold rounded-full bg-lightPurple3 text-white shadow-md hover:bg-purple-900 transition-all">
+                Start Now!
+              </button>
+            </Link>
           </div>
         </div>
+      </div>
     </main>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
