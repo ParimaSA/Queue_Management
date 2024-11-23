8870b5a256946eea21bc097fa0c133f9472b65e7
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-11-23 13:38:47 +0700

    Add new chart and api for analytic section

---
+++ frontend/app/api/analytic/time/route.tsx
@@ -0,0 +1,21 @@
+"use server";
+
+import { DJANGO_API_ENDPOINT } from "@/config/defaults";
+import { NextResponse } from "next/server";
+import ApiProxy from "@/app/api/proxy";
+
+const DJANGO_API_ENTRY_IN_TIME_SLOT_URL = `${DJANGO_API_ENDPOINT}/analytic/time-slot`;
+
+interface ErrorResponse {
+    error: string;
+}
+
+export async function GET() {
+    try {
+        const { data, status }: { data: any; status: number } = await ApiProxy.get(DJANGO_API_ENTRY_IN_TIME_SLOT_URL, true);
+        return NextResponse.json<any>(data, { status });
+    } catch (error) {
+        console.error("Error fetching business:", error);
+        return NextResponse.json<ErrorResponse>({ error: "Failed to fetch business" }, { status: 500 });
+    }
+}
---
+++ frontend/app/business/EstimateDayChart.tsx
@@ -0,0 +1,95 @@
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import { Chart, registerables } from 'chart.js';
+import useSWR from "swr";
+import fetcher from "@/lib/fetcher";
+
+Chart.register(...registerables);
+const AVG_WEEKLY_ENTRY_API_URL = "/api/analytic/day";
+
+interface Entry{
+    day: number;
+    entry_count: number;
+    waiting_time: number;
+}
+
+const EstimateDayChart: React.FC = () => {
+    const chartRef = useRef<HTMLCanvasElement>(null);
+    const chartInstanceRef = useRef<Chart | null>(null);
+    const { data: avg_waiting_time, error: entryError } = useSWR<Entry[]>(AVG_WEEKLY_ENTRY_API_URL, fetcher);
+    const [entryData, setEntryData] = useState<number[]>([0, 0, 0, 0, 0, 0, 0]);
+
+    useEffect(() => {
+        if (entryError) {
+            console.log("Failed to load avg", entryError);
+        } else if (!avg_waiting_time) {
+            console.log("Loading business...");
+        } else {
+            console.log("Avg data:", avg_waiting_time);
+            const updatedEntryData = entryData.map((_, dayIndex) => {
+                const entry = avg_waiting_time.find(entry => entry.day - 1 === dayIndex);
+                return entry ? entry.waiting_time || 0 : 0;
+            });
+            setEntryData(updatedEntryData);
+            console.log("Avg data2:", entryData);
+        }
+    }, [avg_waiting_time, entryError]);
+
+    useEffect(() => {
+        if (chartRef.current) {
+            const ctx = chartRef.current.getContext('2d');
+            if (ctx) {
+                chartInstanceRef.current = new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+                        datasets: [
+                            {
+                                label: 'Average waiting time',
+                                backgroundColor: 'rgba(246, 185, 157)',
+                                data: entryData,
+                            },
+                        ],
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: false,
+                        scales: {
+                            y: {
+                                beginAtZero: true,
+                                title: {
+                                    display: true,
+                                    text: 'Average waiting time (minute)',
+                                    color: '#333',
+                                    font: {
+                                        weight: 'bold'
+                                    },
+                                }
+                            },
+                            x: {
+                                beginAtZero: true,
+                                title: {
+                                    display: true,
+                                    text: 'Day',
+                                    color: '#333',
+                                    font: {
+                                        weight: 'bold'
+                                    },
+                                }
+                            }
+                        },
+                    },
+                });
+            }
+        }
+        return () => {
+            if (chartInstanceRef.current) {
+                chartInstanceRef.current.destroy();
+            }
+        };
+    }, [entryData]);
+
+    return <canvas ref={chartRef} />;
+}
+
+export default EstimateDayChart;
