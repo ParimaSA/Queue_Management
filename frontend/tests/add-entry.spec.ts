86f2e0af49e7b2c75f8c26049aa0742c3f0dc37b
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-24 03:48:15 +0700

    Improve test for adding entry

--- frontend/tests/add-entry.spec.ts
+++ frontend/tests/add-entry.spec.ts
@@ -1,29 +1,30 @@
 import { test, expect } from '@playwright/test';
 
-const baseURL = process.env.TEST_BASE_URL || 'http://localhost:3000'; 
+const baseURL = process.env.TEST_BASE_URL || 'https://queue-management-taupe.vercel.app/'; 
 
 test('Add queue entry and verify queue ticket appears', async ({ page }) => {
   await page.goto(`${baseURL}`);
   
   // Login as Business Owner
   await page.getByRole('button', { name: 'Business Owner' }).click();
-  await page.getByPlaceholder('Username').fill('owner1');
+  await page.getByPlaceholder('Username').fill('PwTest');
   await page.getByPlaceholder('Password').fill('hackme11');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  // Add Dine-in Entry
-  await page.locator('div').filter({ hasText: /^Dine-inPick-upDrive-thruDeliveryAdd$/ }).getByRole('button').click();
-
-  // Verify the "Quueue Name: Dine-in" is visible
-  const dineInText = page.getByText('Queue Name: Dine-in');
-  await expect(dineInText).toBeVisible();
-
   // Add Delivery Entry
-  await page.locator('div').filter({ hasText: /^Dine-inPick-upDrive-thruDeliveryAdd$/ }).getByRole('button').click();
-  await page.getByRole('combobox').selectOption('8');
-  await page.locator('div').filter({ hasText: /^Dine-inPick-upDrive-thruDeliveryAdd$/ }).getByRole('button').click();
+  await page.getByRole('combobox').selectOption('140');
+  await page.locator('div').filter({ hasText: /^ReservationWalk-inTakeawayDeliveryOrder PickupPaymentAdd$/ }).getByRole('button').click();
+  
+  // Verify the "Quueue Name: Delivery" is visible
+  const DeliveryText = page.getByText('Queue Name: Delivery');
+  await expect(DeliveryText).toBeVisible();
 
-  // Verify the "Queue Name: Delivery" is visible
-  const driveThruText = page.getByText('Queue Name: Delivery');
-  await expect(driveThruText).toBeVisible();
+  // Add Takeaway Enrty
+  await page.getByRole('combobox').selectOption('139');
+  await page.locator('div').filter({ hasText: /^ReservationWalk-inTakeawayDeliveryOrder PickupPaymentAdd$/ }).getByRole('button').click();
+
+  // Verify the "Quueue Name: Takeaway" is visible
+  const TakeawayText = page.getByText('Queue Name: Takeaway');
+  await expect(TakeawayText).toBeVisible();
+
 });
