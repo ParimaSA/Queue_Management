59b91a18b8a2965e41865c33815544bfca3a1fce
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-24 04:54:56 +0700

    Improve test for logging in

--- frontend/tests/business-login.spec.ts
+++ frontend/tests/business-login.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-
-const baseURL = process.env.TEST_BASE_URL || 'http://localhost:3000'; 
+const baseURL = process.env.TEST_BASE_URL || 'https://queue-management-taupe.vercel.app/';
 
 test('Business Owner Login and Check Profile', async ({ page }) => {
   await page.goto(`${baseURL}`);
@@ -10,23 +9,23 @@ test('Business Owner Login and Check Profile', async ({ page }) => {
   await page.getByRole('button', { name: 'Business Owner' }).click();
 
   // Login
-  await page.getByPlaceholder('Username').fill('owner1');
+  await page.getByPlaceholder('Username').fill('PwTest');
   await page.getByPlaceholder('Password').fill('hackme11');
   await page.getByRole('button', { name: 'Login' }).click();
 
-  await page.waitForURL(`${baseURL}/business`);
-  await expect(page).toHaveURL(`${baseURL}/business`);
+  await page.waitForURL(`${baseURL}business`);
+  await expect(page).toHaveURL(`${baseURL}business`);
 
   // Go to the profile page
   await page.getByText('Account').click();
   await page.getByRole('link', { name: 'Profile' }).click();
 
-  await page.waitForURL(`${baseURL}/business/profile`);
-  await expect(page).toHaveURL(`${baseURL}/business/profile`);
+  await page.waitForURL(`${baseURL}business/profile`);
+  await expect(page).toHaveURL(`${baseURL}business/profile`);
 
   // Have correct business name
-  const element = page.locator('text="TestRestaurant"');
+  const element = page.locator('text="PwRestaurant"');
   await expect(element).toBeVisible();
-  await expect(element).toHaveText('TestRestaurant');
+  await expect(element).toHaveText('PwRestaurant');
 
 });
