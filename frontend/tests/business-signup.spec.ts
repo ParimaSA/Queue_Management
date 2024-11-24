27b6518cc884da8ca8a79e0527db867fe016a450
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-11-24 17:08:30 +0700

    Improve test for signing up to use unique name

--- frontend/tests/business-signup.spec.ts
+++ frontend/tests/business-signup.spec.ts
@@ -1,32 +1,36 @@
 import { test, expect } from '@playwright/test';
 
-const baseURL = process.env.TEST_BASE_URL || 'http://localhost:3000'; 
+const baseURL = process.env.TEST_BASE_URL || 'https://queue-management-taupe.vercel.app/';
 
 test('Business Owner Sign Up and Login', async ({ page }) => {
+  // Generate unique username and business name
+  const uniqueUsername = 'pwtest' + Date.now();
+  const businessName = 'business' + Date.now();
+
   await page.goto(`${baseURL}`);
 
   // Navigate to Sign-Up page
-  await page.getByRole('button', { name: 'Business Owner' }).click();
+  await page.getByRole('button', { name: 'Start Now' }).click();
   await page.getByRole('link', { name: 'Sign up' }).click();
 
   // Fill in Sign-Up form
   await page.getByPlaceholder('Username').click();
-  await page.getByPlaceholder('Username').fill('owner0');
-  await page.getByPlaceholder('Business Name').fill('TestRestaurant0');
+  await page.getByPlaceholder('Username').fill(uniqueUsername);
+  await page.getByPlaceholder('Business Name').fill(businessName);
   await page.getByPlaceholder('Username').click();
-  await page.locator('#password1').fill('test1234567890'); // Password
-  await page.locator('#password2').fill('test1234567890'); // Confirm Password
+  await page.locator('#password1').fill('hackme11'); // Password
+  await page.locator('#password2').fill('hackme11'); // Confirm Password
 
   // Submit form
   await page.getByRole('button', { name: 'Sign up' }).click();
 
-  await page.waitForURL(`${baseURL}/business/login`);
-  await expect(page).toHaveURL(`${baseURL}/business/login`);
+  await page.waitForURL(`${baseURL}business/login`);
+  await expect(page).toHaveURL(`${baseURL}business/login`);
 
   // Login with new credentials
-  await page.getByPlaceholder('Username').fill('owner0');
+  await page.getByPlaceholder('Username').fill(uniqueUsername);
   await page.getByPlaceholder('Password').click();
-  await page.getByPlaceholder('Password').fill('test1234567890');
+  await page.getByPlaceholder('Password').fill('hackme11');
   await page.getByRole('button', { name: 'Login' }).click();
 
   // Navigate to Profile page
@@ -35,6 +39,6 @@ test('Business Owner Sign Up and Login', async ({ page }) => {
   await page.getByRole('link', { name: 'Profile' }).click();
 
   // Verify correct business name
-  const businessNameElement = page.getByText('TestRestaurant0');
+  const businessNameElement = page.getByText(businessName);
   await expect(businessNameElement).toBeVisible();
 });
