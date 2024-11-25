aa401e2f8379fd66bb1f0614ef29ebcfa6d726e6
Thanchida <thanchida.a@ku.th>
2024-11-25 10:49:53 +0700

    Implement delete all queue button

--- frontend/app/business/AddQueue.tsx
+++ frontend/app/business/AddQueue.tsx
@@ -5,11 +5,20 @@ import { toast } from "react-toastify";
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
 import PreviewIcon from '@mui/icons-material/Preview';
 import Preview from './Preview';
+import useSWR, { mutate } from 'swr';
+import fetcher from '@/lib/fetcher';
 
 const BUSINESS_QUEUE_API_URL = "/api/business/queues";
+const QUEUE_API_URL = "/api/queue/";
 
 interface AddQueueProps {
   onQueueAdded: () => void;
+}
+
+interface Queue {
+  id: number; 
+  name: string;
+  prefix: string;
 }
 
 
@@ -21,6 +30,7 @@ const AddQueue: React.FC<AddQueueProps> = ({ onQueueAdded }) => {
   const [isPreview, setIsPreview] = useState(false)
   const [selectedTemplate, setSelectedTemplate] = useState<string | null>(null);
   const [allPass, setAllPass] = useState(true)
+  const { data: queueData, error: queueError } = useSWR<Queue[]>(BUSINESS_QUEUE_API_URL, fetcher);
   const Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   const hospitalQueues = [
     'Heart', 'Cancer', 'Bone & Spine', 'Brain', 'Trauma', 'Health Check-up', 
@@ -209,6 +219,28 @@ const AddQueue: React.FC<AddQueueProps> = ({ onQueueAdded }) => {
     }
   };
 
+  const handleDeleteAll = async () => {
+    try {
+      for (const queue of queueData || []) {
+        console.log("delete: ", queue.name);
+        const response = await fetch(`${QUEUE_API_URL}/${queue.id}`, {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+  
+        if (!response.ok) {
+          console.error("Failed to delete queue");
+          return;
+        }
+      }
+      mutate(BUSINESS_QUEUE_API_URL);
+    } catch (error) {
+      console.error("Error occurred while deleting queues:", error);
+    }
+  };
+  
   return (
     <>
       <dialog id="my_modal_3" className="modal">
@@ -354,6 +386,11 @@ const AddQueue: React.FC<AddQueueProps> = ({ onQueueAdded }) => {
           <path fillRule="evenodd" d="M12 3.75a.75.75 0 0 1 .75.75v6.75h6.75a.75.75 0 0 1 0 1.5h-6.75v6.75a.75.75 0 0 1-1.5 0v-6.75H4.5a.75.75 0 0 1 0-1.5h6.75V4.5a.75.75 0 0 1 .75-.75Z" clipRule="evenodd" />
         </svg>
           Add Queue
+        </button>
+        <button className="btn bg-lightPurple1 hover:bg-purple rounded-full" onClick={handleDeleteAll}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+        </svg>
         </button>
       </div>
     </>
