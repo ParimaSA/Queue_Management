9f8fb9e40928162bc702e023b54471ff8ef5b44b
Natcha Ranchan <natcha.ran@ku.th>
2024-11-29 16:32:09 +0700

    remove conflict storages

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -206,7 +206,7 @@ AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
 AWS_S3_FILE_OVERWRITE = False
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STORAGES = {
     # media (image storage)
