8a2d7ca4fddd4303b285bbf9ee0a62bee1a251f2
Natcha Ranchan <natcha.ran@ku.th>
2024-11-30 22:31:52 +0700

    test_get_entry_number

--- backend/my_queue/tests/test_analytic.py
+++ backend/my_queue/tests/test_analytic.py
@@ -9,13 +9,13 @@ from my_queue.models import Entry, Business, Queue
 
 
 class Analytic(BaseTestCase):
-    @classmethod
-    def setUp(cls):
+
+    def setUp(self):
         """Create setup data for tests."""
 
         User.objects.create_user(username="owner1", password="123")
-        
-        business = Business.objects.create(
+
+        self.business = Business.objects.create(
             user=User.objects.get(username="owner1"), name="Teenoi")
 
         # business_names = ["Teenoi", "Sushiro"]
@@ -28,7 +28,7 @@ class Analytic(BaseTestCase):
 
             # Create queue
             queue = Queue.objects.create(
-                business=business, name=queue_type, prefix=queue_type[0]
+                business=self.business, name=queue_type, prefix=queue_type[0]
             )
 
             if queue_type == "Big":
@@ -40,7 +40,7 @@ class Analytic(BaseTestCase):
                 entry_time = base_time + entry_counter * time_increment
                 Entry.objects.create(
                     queue=queue,
-                    business=business,
+                    business=self.business,
                     time_in=entry_time,
                     time_out=entry_time + timedelta(minutes=30),
                     status="completed",
@@ -65,9 +65,11 @@ class Analytic(BaseTestCase):
             headers={"Authorization": f"Bearer {token}"},
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.json()[0], {'id': 1, 'name': 'Big', 'prefix': 'B'})
-        self.assertEqual(response.json()[1], {'id': 2, 'name': 'Small', 'prefix': 'S'})
-        
+        self.assertEqual(response.json()[0], {
+                         'id': 1, 'name': 'Big', 'prefix': 'B'})
+        self.assertEqual(response.json()[1], {
+                         'id': 2, 'name': 'Small', 'prefix': 'S'})
+
     def test_get_top_queue_no_business(self):
         """Test that an error message is shown if the user has no business."""
         User.objects.create_user(username="testuser", password="test1234")
@@ -77,7 +79,7 @@ class Analytic(BaseTestCase):
             headers={"Authorization": f"Bearer {token}"},
         )
         self.no_business(response)
-    
+
     def test_analytic_in_time_slot(self):
         """Test that the number of entries in a time slot and waiting time is returned."""
         token = self.login(username="owner1", password="123")
@@ -88,8 +90,45 @@ class Analytic(BaseTestCase):
         data = response.json()
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data[1]["start_time"], 8)
-        
-        avg_sec = sum([(e.time_out-e.time_in).total_seconds() for e in Entry.objects.all()]) / len(data)
+
+        avg_sec = sum([(e.time_out-e.time_in).total_seconds()
+                      for e in Entry.objects.all()]) / len(data)
         avg_waiting_mins = avg_sec / 60
-        self.assertAlmostEqual(data[1]["estimate_waiting"], avg_waiting_mins, delta=5.0)
+        self.assertAlmostEqual(
+            data[1]["estimate_waiting"], avg_waiting_mins, delta=5.0)
         self.assertEqual(data[1]["num_entry"], 8)
+
+    def test_analytic_in_time_slot_no_business(self):
+        """Test that an error message is shown if the user has no business."""
+        User.objects.create_user(username="testuser", password="test1234")
+        token = self.login(username="testuser", password="test1234")
+        response = self.client.get(
+            "/api/analytic/time-slot",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        self.no_business(response)
+
+    def test_get_entry_number(self):
+        Entry.objects.create(
+                    queue=Queue.objects.get(name="Big"),
+                    business=self.business,
+                    time_in=make_aware(datetime(2024, 1, 1, 13, 0, 0)),
+                    status="cancel",
+                )
+        Entry.objects.create(
+                    queue=Queue.objects.get(name="Big"),
+                    business=self.business,
+                    time_in=make_aware(datetime(2024, 1, 1, 13, 5, 0)),
+                    status="waiting",
+                )
+        token = self.login(username="owner1", password="123")
+        response = self.client.get(
+            "/api/analytic/entry",
+            headers={"Authorization": f"Bearer {token}"},
+        )
+        self.assertEqual(response.json()["total_entry"],10)
+        self.assertEqual(response.json()["waiting_entry"],1)
+        self.assertEqual(response.json()["cancel_entry"],1)
+        self.assertEqual(response.json()["complete_entry"],8)
+        
+    
