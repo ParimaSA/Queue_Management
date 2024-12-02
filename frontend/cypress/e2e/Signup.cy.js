168a1e607abb0e74eb201ee2d93737b58faa50a8
Nichapat Sirinantanakul <nichapat.sirin@ku.th>
2024-12-02 18:48:20 +0700

    Use login command

--- frontend/cypress/e2e/Signup.cy.js
+++ frontend/cypress/e2e/Signup.cy.js
@@ -8,37 +8,34 @@ describe('Signup', () => {
     });
 
     it('Sign Up and Login Flow', () => {
-      // Navigate to Sign-Up page
-      cy.contains('button', 'Start Now').click();
-      cy.contains('a', 'Sign up').click();
-  
-      // Ensure the page is fully loaded
-      cy.get('input[placeholder="Username"]').should('be.visible');
-  
-      // Fill the sign-up form
-      cy.get('input[placeholder="Username"]').type(uniqueUsername);
-      cy.get('input[placeholder="Business Name"]').type(businessName);
-      cy.get('#password1').type('hackme11'); // Password
-      cy.get('#password2').type('hackme11'); // Confirm Password
-  
-      // Submit the form
-      cy.contains('button', 'Sign up').click();
-  
-      // Verify redirection to login page
-      cy.url().should('include', `${baseURL}business/login`);
-  
-      // Login with new credentials
-      cy.get('input[placeholder="Username"]').type(uniqueUsername);
-      cy.get('input[placeholder="Password"]').type('hackme11');
-      cy.contains('button', 'Login').click();
-  
-      // Navigate to Profile page
-      cy.contains('summary', 'Account').click();
-      cy.contains('a', 'Profile').click();
-  
-      // Verify correct business name
-      cy.contains(businessName).should('be.visible');
+        // Navigate to Sign-Up page
+        cy.contains('button', 'Start Now').click();
+        cy.contains('a', 'Sign up').click();
+    
+        // Ensure the page is fully loaded
+        cy.get('input[placeholder="Username"]').should('be.visible');
+    
+        // Fill the sign-up form
+        cy.get('input[placeholder="Username"]').type(uniqueUsername);
+        cy.get('input[placeholder="Business Name"]').type(businessName);
+        cy.get('#password1').type('hackme11'); // Password
+        cy.get('#password2').type('hackme11'); // Confirm Password
+    
+        // Submit the form
+        cy.contains('button', 'Sign up').click();
+    
+        // Verify redirection to login page
+        cy.url().should('include', `${baseURL}business/login`);
+    
+        // Login with new credentials
+        cy.login(uniqueUsername, 'hackme11')
+    
+        // Navigate to Profile page
+        cy.contains('summary', 'Account').click();
+        cy.contains('a', 'Profile').click();
+    
+        // Verify correct business name
+        cy.contains(businessName).should('be.visible');
     });
   });
-  
-  
+   
